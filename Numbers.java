3a65c7b6383f6b37e0167d22e387f1d180a15e84
kalpakurisaipraneeth <navyakalpakuri@gmail.com>
2020-07-26 22:19:36 +0530

    assignment

---
+++ Numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Numbers {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n;
+		n=scan.nextInt();
+		for(int i=1 ;i<=n;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
